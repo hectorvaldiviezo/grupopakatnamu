e9fd7e618e66d0080ecf1d8eadde6a3718afdd51
hectorvaldiviezo <hvaldiviezos@transportespakatnamu.com>
2025-04-21 18:18:22 -0500

    Update page.tsx

--- app/libro-reclamaciones/consulta/page.tsx
+++ app/libro-reclamaciones/consulta/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import ComplaintQuery from "@/components/complaints/components/ComplaintQuery";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
 import Hero from "@/components/Hero";
 import { MILLA_BASE } from "@/lib/config";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
+
 export const dynamic = "force-dynamic";
-export default function Page() {
+
+function ComponentConsultaReclamaciones() {
   const params = useSearchParams();
   const [complaintCode, setComplaintCode] = useState<string | undefined>(
     undefined
@@ -27,24 +27,33 @@ export default function Page() {
     }
   }, [params]);
 
+  return <ComplaintQuery complaintCodeParam={complaintCode} error={error} />;
+}
+
+function Skeleton() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <main>
-        <Hero
-          title="LIBRO DE RECLAMACIONES"
-          subtitle=""
-          description=""
-          src={
-            MILLA_BASE +
-            "/webImages/4/Libro de Reclamaciones"
-          }
-          height="h-[400px]"
-          gradient={true}
-        />
-        <ComplaintQuery complaintCodeParam={complaintCode} error={error} />
-      </main>
-      <Footer />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="animate-pulse bg-gray-300 h-10 w-1/2 rounded"></div>
+      <div className="mt-4 animate-pulse bg-gray-300 h-10 w-1/4 rounded"></div>
+      <div className="mt-4 animate-pulse bg-gray-300 h-10 w-1/4 rounded"></div>
     </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <main>
+      <Hero
+        title="LIBRO DE RECLAMACIONES"
+        subtitle=""
+        description=""
+        src={MILLA_BASE + "/webImages/Libro de Reclamaciones"}
+        height="h-[300px]"
+        gradient={true}
+      />
+      <Suspense fallback={<Skeleton />}>
+        <ComponentConsultaReclamaciones />
+      </Suspense>
+    </main>
   );
 }
